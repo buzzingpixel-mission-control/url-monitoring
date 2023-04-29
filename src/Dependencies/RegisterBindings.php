bbd257731586b5b4496f91c82e0369b817b53aa4
TJ Draper <tj@buzzingpixel.com>
2023-04-28 23:27:55 -0500

    Set up binding registration and event registration

---
+++ src/Dependencies/RegisterBindings.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MissionControlUrlMonitoring\Dependencies;
+
+use MissionControlBackend\ContainerBindings;
+
+class RegisterBindings
+{
+    public static function register(ContainerBindings $containerBindings): void
+    {
+    }
+}
---
+++ src/EventListeners/EventRegistration.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MissionControlUrlMonitoring\EventListeners;
+
+use Crell\Tukio\OrderedProviderInterface;
+
+class EventRegistration
+{
+    public static function register(OrderedProviderInterface $provider): void
+    {
+    }
+}
